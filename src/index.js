e73942a81499071543f5867751694440de67fc96
Sridhar GS <sridhar.cse12@gmail.com>
2021-01-20 14:08:40 +0530

    modified structure cra

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './containers/Root';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './store';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
